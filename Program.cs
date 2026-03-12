7db9b71e7b2e2d1068eb56cfd0ddb5406070bdfc
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2026-03-12 10:35:43 +0100

    test commit, braches reposition

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace exercices
             Pareja<string, int> parStr = new Pareja<string, int>("AAAAA", 1234);
             Pareja<double, bool> parBool = new Pareja<double, bool>(2.5, true);
 
+            //WL
             Console.WriteLine(parStr.ToString(), parStr.Valor1, parStr.Valor2);
             Console.WriteLine(parBool.ToString(), parBool.Valor1, parBool.Valor2);
         }
