001798b70eef6303f311a710734ef69c1d9a7061
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2026-03-12 10:32:16 +0100

    solution of exercise 1

---
+++ Pareja.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices
+{
+    public class Pareja<T1, T2>
+    {
+        public T1? Valor1 { get; set; }
+
+        public T2? Valor2 { get; set; }
+
+        public Pareja(T1? str, T2? integer)
+        {
+            Valor1 = str;
+            Valor2 = integer;
+        }
+
+        public override string ToString() => "First value = {0}, second value = {1}";
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Exercices;
+
+namespace exercices
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Pareja<string, int> parStr = new Pareja<string, int>("AAAAA", 1234);
+            Pareja<double, bool> parBool = new Pareja<double, bool>(2.5, true);
+
+            Console.WriteLine(parStr.ToString(), parStr.Valor1, parStr.Valor2);
+            Console.WriteLine(parBool.ToString(), parBool.Valor1, parBool.Valor2);
+        }
+    }
+}
